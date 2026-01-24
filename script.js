27147e8ab3673cce91c5308c3dde1ac40ca2de44
tochkalolzteam-lang <tochkalolzteam@gmail.com>
2026-01-24 12:50:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -51,3 +51,20 @@ function updateStock() {
 }
 
 updateStock();
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Запускаем один раз при загрузке, чтобы проверить первый экран
+reveal();
